8a6e0fb5bf7c61a727e80c5ed47943d34ec97cd1
gobeecode <gopalakrishnan.work@gmail.com>
2025-09-26 15:30:58 +0530

    vault backup: 2025-09-26 15:30:58

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
+            to="/docs/gobeecode">
             Explore 🧑‍💻
           </Link>
         </div>
